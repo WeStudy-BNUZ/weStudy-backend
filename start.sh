af8e4b5cd119b77afc35f509db06ec770abb7ce3
Sihanlay <1463837917@qq.com>
2020-11-22 14:38:57 +0800

    ??

--- start.sh
+++ start.sh
@@ -9,7 +9,11 @@ cat ./go.mod
 
 # linux环境编译
 #CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o main
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build main.go
+#CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build main.go
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build main.go
 # 构建docker镜像，项目中需要在当前目录下有dockerfile，否则构建失败
 docker build -t demo .
 #rm -rf main
