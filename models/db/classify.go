d60ba86769846c206ed57dc34df3f1e79a26da6a
satsun111 <315632182@qq.com>
2020-11-24 21:11:11 +0800

    classify

--- models/db/classify.go
+++ models/db/classify.go
@@ -8,5 +8,5 @@ type College struct{
 type Major struct{
 	ID int `gorm:"primary_key" json:"id"`
 	Name string `json:"name" gorm:"not null"`
-	CollegeID int `json:"college_id"`
+	CollageID int `json:"collage_id"`
 }
